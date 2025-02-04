4025d2e8d8c6a36aae083c28080ed2f39821162c
kagura7799 <mkotelnikov97@gmail.com>
2025-02-04 21:43:08 +0300

    added GetFileVersionInfoA for correct display of some window

--- core/tracking/ActiveWindowTracker.cpp
+++ core/tracking/ActiveWindowTracker.cpp
@@ -1,40 +1,101 @@
 #include "ActiveWindowTracker.hpp"
+#include "gui/timetrackerapps.h"
 #include <iostream>
 #include <QDebug>
+#include <QString>
 
-ActiveWindowTracker::ActiveWindowTracker()
+ActiveWindowTracker::ActiveWindowTracker() : previousWindowTitle("")
 {
+
 }
 
-std::wstring ActiveWindowTracker::getActiveWindowName()
+std::string ActiveWindowTracker::getProcessPath()
 {
     HWND hWin = GetForegroundWindow();
     if (!hWin)
-        return L"";
+        return "";
 
-    wchar_t windowTitle[256];
-    int length = GetWindowTextW(hWin, windowTitle, 256);
+    DWORD pid;
+    GetWindowThreadProcessId(hWin, &pid);
 
-    if (length <= 0) 
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_LIMITED_INFORMATION, FALSE, pid);
+    if (!hProcess) return "unknown";
+
+    char filename[MAX_PATH] = { 0 };
+    DWORD size = MAX_PATH;
+
+    if (!QueryFullProcessImageNameA(hProcess, 0, filename, &size))
     {
-        return L"";
+        return "unknown";
     }
 
-    std::wstring fullTitle(windowTitle, length);
+    CloseHandle(hProcess);
 
-    // symbol followed by the title
-    size_t pos = fullTitle.rfind(L" - ");
-    
-    // cut off the line leaving the title
-    if (pos != std::wstring::npos)
-    {
-        return fullTitle.substr(pos + 3);
-    }
-
-    return fullTitle;
+    return std::string(filename);
 }
 
-void ActiveWindowTracker::switchActiveWindowForTimer()
+std::string ActiveWindowTracker::getFullActiveAppName()
 {
+    std::string filePath = getProcessPath();
 
+    DWORD dummy;
+    DWORD size = GetFileVersionInfoSizeA(filePath.c_str(), &dummy);
+    if (size == 0) return "unknown";
+
+    std::vector<BYTE> buffer(size);
+    if (!GetFileVersionInfoA(filePath.c_str(), 0, size, buffer.data())) return "";
+
+    VS_FIXEDFILEINFO* fileInfo;
+    UINT len;
+    if (!VerQueryValueA(buffer.data(), "\\", (LPVOID*)&fileInfo, &len)) return "";
+
+    char* productName = nullptr;
+    if (VerQueryValueA(buffer.data(), "\\StringFileInfo\\040904b0\\FileDescription", (LPVOID*)&productName, &len)) 
+    {
+        return std::string(productName);
+    }
+
+    return "unknown";
 }
+
+std::string ActiveWindowTracker::handleTitleString(std::string str)
+{
+    size_t pos = str.find(".exe");
+    if (pos != std::string::npos)
+    {
+        str.erase(pos, 4);
+    }
+
+    size_t lastSlash = str.find_last_of("\\/");
+    if (lastSlash == std::string::npos) return str;
+
+    return str.substr(lastSlash + 1);
+}
+
+void ActiveWindowTracker::trackActiveWindowTime(timetrackerapps* window)
+{
+    timer.tick();
+
+    std::string currentWindowTitle = getFullActiveAppName();
+
+    // if app version was not found, go to get process name
+    if (currentWindowTitle == "unknown")
+        currentWindowTitle = handleTitleString(getProcessPath());
+
+    std::wstring wCurrentWindowTitle(currentWindowTitle.begin(), currentWindowTitle.end());
+    std::string time = timer.currentTime();
+
+    if (currentWindowTitle != previousWindowTitle)
+    {
+        qInfo() << "Active window changed: " << QString::fromStdString(currentWindowTitle);
+        addItem(time, wCurrentWindowTitle, window);
+        previousWindowTitle = currentWindowTitle;
+    }
+}
+
+void ActiveWindowTracker::addItem(std::string time, std::wstring title, timetrackerapps* window)
+{
+    window->addWindowItem(QString::fromStdWString(title), QString::fromStdString(time));
+}
+
+#pragma comment(lib, "version.lib")
--- core/tracking/ActiveWindowTracker.hpp
+++ core/tracking/ActiveWindowTracker.hpp
@@ -1,11 +1,20 @@
 #include <Windows.h>
 #include <psapi.h>
 #include <string>
+#include "gui/timetrackerapps.h"
+#include "core/timer/Timer.hpp"
 
 class ActiveWindowTracker
 {
 public:
 	ActiveWindowTracker();
-	void switchActiveWindowForTimer();
-	std::wstring getActiveWindowName();
+	void trackActiveWindowTime(timetrackerapps* window);
+	std::string getProcessPath();
+	std::string getFullActiveAppName();
+
+private:
+	void addItem(std::string time, std::wstring title, timetrackerapps* window);
+	Timer timer;
+	std::string previousWindowTitle;
+	std::string handleTitleString(std::string str);
 };
