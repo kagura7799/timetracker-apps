6616178484c80ab2ceb77f5d4291088f8471d091
kagura7799 <mkotelnikov97@gmail.com>
2025-02-04 21:28:31 +0300

    using stack instead of heap

--- app/main.cpp
+++ app/main.cpp
@@ -1,10 +1,24 @@
-#include "gui\timetrackerapps.h"
+#include "gui/timetrackerapps.h"
+#include "core/tracking/ActiveWindowTracker.hpp"
 #include <QApplication>
+#include "QDebug"
+#include <QTimer>
 
 int main(int argc, char* argv[])
 {
     QApplication a(argc, argv);
     timetrackerapps w;
+
+    ActiveWindowTracker winTracker;
+
+    QTimer* timer = new QTimer();
+
+    QObject::connect(timer, &QTimer::timeout, [&, w_ptr = &w]() {
+        winTracker.trackActiveWindowTime(w_ptr);
+    });
+
+    timer->start(1000);
+
     w.show();
     return a.exec();
 }
